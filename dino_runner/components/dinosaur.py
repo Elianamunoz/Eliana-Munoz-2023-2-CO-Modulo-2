c75c5f7c817079ac7544782a26862cc2a5309089
Elianamunoz <129460594+Elianamunoz@users.noreply.github.com>
2023-04-05 05:08:14 -0500

    four-class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -44,7 +44,6 @@ class Dinosaur(Sprite):
         if self.step_index >= 10:
             self.step_index = 0
 
-
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect = self.image.get_rect()
@@ -69,6 +68,16 @@ class Dinosaur(Sprite):
         self.dino_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
-
     def draw(self,screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def reset_dinosaur(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_jump = False
+        self.dino_duck = False
+        self.jump_speed = self.JUMP_SPEED
